6c038adfdcf3f959e988e494e58a48fba5727e30
Bruce <bruce.hillsworth@gmail.com>
2023-03-31 16:08:18 -0600

    Added eventListener to addToyForm element, added fetch POST to save new toy in database, and pushed new toy to createToyCard function to add to main page

--- src/index.js
+++ src/index.js
@@ -5,34 +5,38 @@ document.addEventListener("DOMContentLoaded", () => {
   const addToyForm = document.querySelector("form.add-toy-form");
   let addToy = false;
 
-  function createToyCard(toysObj) {
-    toysObj.forEach((toy) => {
-      const div = document.createElement("div");
-      const hTwo = document.createElement("h2");
-      const img = document.createElement("img");
-      const p = document.createElement("p");
-      const btn = document.createElement("button");
-      div.setAttribute("class", "card");
-      hTwo.textContent = toy.name;
-      img.setAttribute("src", toy.image);
-      img.setAttribute("class", "toy-avatar");
-      p.textContent = `${toy.likes} Likes`;
-      btn.setAttribute("class", "like-btn");
-      btn.setAttribute("id", toy.id);
-      btn.textContent = "Like ❤️";
-      div.appendChild(hTwo);
-      div.appendChild(img);
-      div.appendChild(p);
-      div.appendChild(btn);
-      toyCollectionContainer.appendChild(div);
-    });
+  //Functions
+
+  function createToyCard(toy) {
+    const div = document.createElement("div");
+    const hTwo = document.createElement("h2");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    const btn = document.createElement("button");
+    div.setAttribute("class", "card");
+    hTwo.textContent = toy.name;
+    img.setAttribute("src", toy.image);
+    img.setAttribute("class", "toy-avatar");
+    p.textContent = `${toy.likes} Likes`;
+    btn.setAttribute("class", "like-btn");
+    btn.setAttribute("id", toy.id);
+    btn.textContent = "Like ❤️";
+    div.appendChild(hTwo);
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(btn);
+    toyCollectionContainer.appendChild(div);
   }
+
+  //Fetches all toys or main
 
   fetch("http://localhost:3000/toys")
     .then((response) => response.json())
     .then((toysObj) => {
-      createToyCard(toysObj);
+      toysObj.forEach((toy) => createToyCard(toy));
     });
+
+  //eventListeners
 
   addBtn.addEventListener("click", () => {
     addToy = !addToy;
@@ -42,4 +46,27 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  addToyForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    const newToyToPost = {
+      name: event.target.name.value,
+      image: event.target.image.value,
+      likes: 0,
+    };
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newToyToPost),
+    };
+    fetch("http://localhost:3000/toys", configurationObject)
+      .then((response) => response.json())
+      .then((toy) => {
+        createToyCard(toy);
+      });
+  });
+  addToyForm.reset();
 });
